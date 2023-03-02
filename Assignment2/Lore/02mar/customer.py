7f4b703dc2bda459e4bf7015740e60d36e688ab4
Lore Verstraete <l.c.w.a.verstraete@student.tue.nl>
2023-03-02 12:40:26 +0100

    description of assignment 2

---
+++ Assignment2/Lore/02mar/customer.py
@@ -0,0 +1,17 @@
+class customer:
+    # arrival rate of the customers - group size of those arrivals, which is geometrically distributed with mean 3
+    # time in between arrival and queueing 
+
+    def arrive(meangroupsize, poissonrate):
+        # calculate group size (geometric)
+        # calculate arrival rate (poisson)
+        # output arrival times per person
+
+    def take_food(mean):
+        # exponentially distributed amount of time with mean of 80 seconds
+        # output how long it takes in between arrival and queue
+
+    def cardcash(percentagecash):
+        # 40% cash
+        # output cash or card
+
---
+++ Assignment2/Lore/02mar/service.py
@@ -0,0 +1,13 @@
+from customer import customer
+
+class service:
+    # This class determines the service times, which are exponentially distributed
+    # with means 20 and 12 seconds for payment by cash and card, respectively. It has
+    # been observed that 40 % of customers pay cash.
+    # 3 cashiers
+        
+    def queue()
+        # pick shortest queue
+        # output queue
+
+    def service(mean, cardcash)
