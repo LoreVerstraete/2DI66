58fb4db3f67e2eff17bc1e29916f9398724a9037
Paulina <p.huett@outlook.com>
2023-03-23 12:00:58 +0100

    groupwork 1 commit

--- Assignment3/Paulina/23mar/Elevator.py
+++ Assignment3/Paulina/23mar/Elevator.py
@@ -3,17 +3,15 @@ from Customer import Customer
 class Elevator:
     ''' Describes the elevator processes '''
     
-    MOVETIME = 6
-    MAXPEOPLE = 10
-    FLOORS = 4
+    moveTime = 6
+    maxPeople = 10
+    floors = 4
 
     def __init__(self,elevatorNumber):
         self.elevatorNumber = elevatorNumber
         self.numberOfPeople = 0 # at the start there are no people in the elevators 
         self.floor = 0 # all th elevators start start at the ground floor 
         self.directionUp = True # if moving down than false 
-        self.destinationFloor = []
-        self.stopElevator = False
 
 
     def movingDoors(self, doorDist):  #time of the door openong/closing 
@@ -31,15 +29,11 @@ class Elevator:
         Calculates the new floor.
         """
         if directionUp:
-            self.floor += 1
-            if self.floor == 4:
-                self.directionUp= not directionUp
+            return floornumber + 1
         else:
-            self.floor = floornumber - 1
+            return floornumber - 1
 
-    
-    def checkStop(self, floor, destinationFloor):
-        self.stopElevator = destinationFloor.count(floor)
+        
 
 '''
     def stopElevator(self, floor, customer):
---
+++ Assignment3/Paulina/23mar/Simulation.py
@@ -0,0 +1,181 @@
+from Customer import Customer
+from Elevator import Elevator
+from Distribution import Distribution
+# from FCFSQueue import FCFSQueue
+from scipy import stats
+from collections import deque
+from Results import Results
+from numpy import *
+from Event import Event 
+from FES import FES 
+import random
+
+class Simulation:
+    ''' Describes the simulation '''
+    def __init__(self, arrDist, doorDist, nrElevators): #, arrDist1, arrDist2, arrDist3, arrDist4, doorDist, nrElevators):
+        self.arrDist = arrDist
+        self.doorDist = doorDist
+        self.nrElevators = nrElevators
+        self.probFloor = probFloor 
+
+
+    def simulate(self, T):
+        fes = FES()
+        res = Results()
+        queue = [deque() for i in arange(5)] # for the first floor 
+        queueElevator = deque()
+        S = 0 # queue length integrated  
+        q = 0 # queue length 
+        t = 0 # time at the beginning 
+        a = self.arrDist[0].rvs()  # samples number of the next arrival 
+        print("first arrival", a)
+        firstEvent = Event(Event.ARRIVAL, a, 0, 1)  # schedule first event 
+        secondEvent = Event(Event.ELEVATORSTOPS, 0)
+        elevator = Elevator(1)
+        fes.add(firstEvent) # adding first event to future events 
+        fes.add(secondEvent)
+        round=0
+        while t<T: 
+            print("  ")
+            print("round", round)
+            round +=1
+            e = fes.next()  # taking first element of the fes list and deleting this event 
+            t = e.time
+            print(e)
+            if e.type == Event.ELEVATORSTOPS:
+                f = elevator.floor
+                print("floor: ",f)
+                print('t start', t)
+                
+                if len(queueElevator) > 0 or len(queue[f]) > 0:
+                # while len(queueElevator) > 0:
+                    t += self.doorDist.rvs()
+                    removeCustomers = []
+                    for i in queueElevator:
+                        # i = queueElevator[0]
+                        print("customer", i)
+                        if i.destinationFloor == f:
+                            removeCustomers.append(i)
+                    for k in removeCustomers:
+                        queueElevator.remove(k)
+                    t += 1     
+                                   
+                    
+                    if len(queueElevator) < elevator.maxPeople:
+                        for a in arange(5):
+                            if a == f:
+                                print("right floor", "a", a, "f", f)
+                                print("len q", len(queue[a]))
+                                while len(queue[a]) > 0:
+                                    print("only if queue")
+                                    c = queue[a][0]
+                                    queueElevator.append(c)
+                                    t += 1
+                                    queue[a].remove(c)
+                                    
+                    t += self.doorDist.rvs()                            
+                print("t end", t)  
+                if elevator.floor == 1:
+                    elevator.floor -= 1
+                else: 
+                    elevator.floor += 1
+                nextFloor = Event(Event.ELEVATORSTOPS, t+6)
+                fes.add(nextFloor)
+                
+                
+            if e.type == Event.ARRIVAL: # arrival of a customer
+                for a in arange(5):
+                    if e.floor == a:
+                        des = random.choices(range(5), weights = probFloor[0], k = 1)[0] #random.randint(0,1)
+                        print("des", des)
+                        c = Customer(t, des, a)
+                        queue[a].append(c)
+                        print("queue", queue[a])
+                        print(c)
+                        a = self.arrDist[0].rvs() 
+                        nextCustomer = Event(Event.ARRIVAL, t+a)
+                        fes.add(nextCustomer) 
+            # if e.type == Event.ARRIVAL1:
+            #     for a in arange(5):
+            #         if e.floor == a:
+            #             des = random.randint(0,1)
+            #             print("des", des)
+            #             c = Customer(t, des, a)
+            #             queue[a].append(c)
+            #             print("queue", queue[a])
+            #             print(c)
+            
+                            
+                    
+            
+            
+            # if e.type == Event.ARRIVAL: 
+            #     c = Customer(t, 1 ,0)
+            #     print(c)
+            #     queue.append(c)
+            #     doorDist = self.doorDist.rvs()
+            #     t += doorDist # Elevator.movingDoors(doorDist) # opend door time 
+                
+            #     print("door",doorDist)
+            #     if  elevator.numberOfPeople < elevator.maxPeople:
+            #         queue.remove(c)
+            #         queueElevator.append(c)
+            #         t += 1
+            #     doorDist = self.doorDist.rvs()
+            #     t += doorDist
+            #     print("door", doorDist)
+            #     a = self.arrDist0.rvs() 
+            #     nextEvent = Event(Event.ARRIVAL, a+t)
+            #     print(nextEvent)
+            #     fes.add(nextEvent)
+            #     nextEvent = Event(Event.ELEVATORSTOPS, t+ Elevator.moveTime)
+            #     if elevator.floor == 0: # checks if the direction of he elevator needs to change 
+            #         elevator.directionUp = True
+            #     if elevator.floor == 4: 
+            #         elevator.directionUp = False
+            #     elevator.floor = elevator.newFloor(elevator.floor, elevator.directionUp)
+
+            #     print(nextEvent)
+            # elif e.type == Event.ELEVATORSTOPS:
+            #     print("Queue: ",queueElevator)
+            #     if  elevator.numberOfPeople > 0 and c.destinationFloor == 1:
+            #         elevator.floors += 1
+            #         queueElevator.remove(c)
+            #         #print(queueElevator)
+
+            
+
+
+# destinationFloor = random.choices(range(nrStates), weights = p[x[i-1]], k = 1)[0] for x[i]
+# choose floor: x[i] = random.choices(range(nrStates), weights = p[x[i-1]], k = 1)[0]
+
+
+probFloor = array([[0, 0.1, 0.3, 0.4, 0.2],
+             [0.7, 0, 0.1, 0.1, 0.1],
+             [0.6, 0.2, 0, 0.1, 0.1],
+             [0.6, 0.2, 0.1, 0, 0.1],
+             [0.5, 0.2, 0.2, 0.1, 0]])
+
+arrDist0 = Distribution(stats.expon(scale = 60/13.1))
+arrDist1 = Distribution(stats.expon(scale = 60/3.4))
+arrDist2 = Distribution(stats.expon(scale = 60/2.1))
+arrDist3 = Distribution(stats.expon(scale = 60/9.2))
+arrDist4 = Distribution(stats.expon(scale = 60/8.8))
+
+arrDist = [arrDist0, arrDist1, arrDist2, arrDist2, arrDist3, arrDist4]
+
+doorDist = Distribution(stats.expon(scale = 3))
+
+nrElevators = 1 # amount of elevators
+
+#print(arrDist0.rvs())
+sim = Simulation(arrDist, doorDist, nrElevators)
+#print(sim.simulate(3))
+
+sim.simulate(200)
+
+"""         self.arrDist1 = arrDist1
+        self.arrDist2 = arrDist2
+        self.arrDist3 = arrDist3
+        self.arrDist4 = arrDist4 
+"""
